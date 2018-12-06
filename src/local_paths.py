81a0bbaaef2856cbf924158d73ad9bc8797ab149
AlexLamson <alex.lamson@gmail.com>
2018-12-06 17:42:43 -0500

    actually add the path script this time

---
+++ src/local_paths.py
@@ -0,0 +1,16 @@
+from os import listdir
+from os.path import isfile, join
+
+
+models_dir = "../models"
+
+data_dir = '../res'
+# data_dir = 'C:/Users/raffc/Downloads/coco2017'
+
+
+def latest_model(mode="random"):
+    model_fnames = [f for f in listdir(models_dir) if isfile(join(models_dir, f))]
+    model_fnames = [fname for fname in model_fnames if mode in fname]
+    model_fnames = sorted(model_fnames, reverse=True)
+    latest_model_fname = model_fnames[0]
+    return latest_model_fname
