3957a4ab81fe0891a9d8cd82ab182cf24bb9b51e
chrisraff <crharfifs@gmail.com>
2018-12-13 15:11:31 -0500

    updated targets

--- src/targets.py
+++ src/targets.py
@@ -1,2 +1,1 @@
-# target_class_names = "cat dog horse broccoli carrot car couch bed airplane boat".split()
-target_class_names = "cat dog horse broccoli carrot car couch bed airplane".split()
+target_class_names = "cat dog horse broccoli carrot car bicycle boat airplane".split()
